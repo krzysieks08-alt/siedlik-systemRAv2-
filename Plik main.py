3481beff4d6424367defb0711e49ae7cdacd8ebd
krzysieks08-alt <krzysieks08@gazeta.pl>
2026-05-02 04:13:31 +0200

    Simplify main.py by removing unnecessary settings
    Removed title and theme mode settings from the page.

--- Plik main.py
+++ Plik main.py
@@ -1,10 +1,8 @@
+
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "SIEDLIK SYSTEM"
-    page.theme_mode = ft.ThemeMode.DARK
-    page.add(ft.Text("SYSTEM DZIAŁA!", size=30))
-    page.add(ft.ElevatedButton("GENERUJ SYGNAŁ"))
+    page.add(ft.Text("SIEDLIK SYSTEM DZIAŁA!"))
 
 if __name__ == "__main__":
     ft.app(target=main)
